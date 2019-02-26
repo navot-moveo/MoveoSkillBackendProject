0c21f161e1ae496a46b1637afc6d0f694de1bdde
Navot slavin <navot@moveo.co.il>
2019-02-26 15:16:28 +0200

    update mongooseInit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongo = require('./db/mongooseinit')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
---
+++ db/mongooseinit.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var config = require('config');
+
+//taking the data from config/default.json
+var dbConfig = config.get("MongoDB.Configurations");
+
+var user = dbConfig.user;
+var password = dbConfig.password;
+
+if (typeof password === 'undefined' || password.length <= 0) {
+    password = '';
+} else {
+    password = ':' + password + '@';
+}
+
+//connecting to db
+mongoose.connect('mongodb://' + user + password + dbConfig.host + ':' + dbConfig.port + '/' + dbConfig.database, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log('DB connection success');
+})
+
+module.exports = {
+
+};
