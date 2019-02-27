9e87f58c434510868e32be8d6ffb8e0c86f3e933
navot-moveo <navot@moveo.co.il>
2019-02-27 11:53:30 +0200

    app config changes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongo = require('./db/mongooseinit')
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var apiRoutes = require('./api/v1/routes/apiRoutes');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,9 +17,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
