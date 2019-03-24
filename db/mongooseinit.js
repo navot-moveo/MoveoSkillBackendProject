16d5343a216bd98bda81b60b6fea243ed6c76850
navot-moveo <navot@moveo.co.il>
2019-03-24 13:24:06 +0200

    no message

--- db/mongooseinit.js
+++ db/mongooseinit.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var mongoose = require('mongoose');
+/*
 var config = require('config');
 
 //taking the data from config/default.json
-var dbConfig = config.get("MongoDB.Configurations");
+//var dbConfig = config.get("MongoDB.Configurations");
 
-var user = dbConfig.user;
+//var user = dbConfig.user;
 var password = dbConfig.password;
 
 if (typeof password === 'undefined' || password.length <= 0) {
@@ -14,7 +15,7 @@ if (typeof password === 'undefined' || password.length <= 0) {
 } else {
     password = ':' + password + '@';
 }
-
+*/
 //connecting to db
 // example: mongoose.connect('mongodb://username:password@host:port/database')
 mongoose.connect('mongodb://navot:pato@ec2-34-253-183-22.eu-west-1.compute.amazonaws.com:27017/foodDB');
