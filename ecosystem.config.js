28ec785bd3b8e7e1a5602d9dc8d5fe36448f749c
navot-moveo <navot@moveo.co.il>
2019-03-24 11:44:51 +0200

    ssrsrss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,11 +10,9 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+      PORT: 3000,
+      NODE_ENV: "production"
+     }
   }],
 
   deploy : {
